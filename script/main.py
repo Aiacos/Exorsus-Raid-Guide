edbb47e35ba5f13eabfa019c5024a7c40125b709
Lorenzo <uni.lorenzo.a@gmail.com>
2019-08-22 20:55:48 +0100

    script to automate process

---
+++ script/main.py
@@ -0,0 +1,96 @@
+from bs4 import BeautifulSoup as bs
+import requests
+import re
+
+
+class Converter(object):
+
+    def __init__(self, url):
+        self.greenColor = '|cFF00FF00xxxxxx|r'
+        self.redColor = '|cFFFF0000xxxxxx|r'
+        self.grayColor = '|cff808080xxxxxx|r'
+        self.spellColor = '|cFF72D5FFxxxxxx|r'
+        self.epicColor = '|cFFA335EExxxxxx|r'
+
+
+        r = requests.get(url)
+        soup = bs(r.content, 'html.parser')
+        # print(soup.find_all('h3', string='2.8. Summary for Tanks'))
+        tankSectionTag = soup.find_all('h3', string=re.compile('Summary for Tanks'))[0]
+        healerSectionTag = soup.find_all('h3', string=re.compile('Summary for Healers and DPS'))[0]
+        dpsSectionTag = soup.find_all('h3', string=re.compile('Summary for Healers and DPS'))[0]
+
+        # ToDo: nomeBoss
+        bossName = 'Blackwater Behemoth'
+
+        print self.finalize(bossName, tankSectionTag, healerSectionTag, dpsSectionTag)
+
+    def extractIconID(self, link):
+        id = link.split('/')[-1].split('-')[0]
+        return str(id)
+
+    def findTextIcon(self, tag):
+        pass
+
+    def wrapTextWith(self, text, wrap):
+        s = wrap.replace('xxxxxx', text)
+        return s
+
+    def wrapSpell(self, spellID):
+        s = '{spell:' + spellID + '}'
+        return s
+
+    def contentPrepare(self, contentList):
+        contentTextList = []
+        # check phase
+        if (contentList.name == 'h4'):
+            contentTextList.append(self.wrapTextWith(contentList.text, self.grayColor))
+            contentList = contentList.next_element.next_element.next_element
+
+        # content
+        for li in contentList.children:
+            if (li.string):
+                contentTextList.append(li.string.rstrip('\n'))
+            else:
+                text = li.text
+                linkTagList = li.find_all('a')
+                for linkTag in linkTagList:
+                    href = linkTag['href']
+                    spellName = linkTag.string
+                    text = text.replace(spellName, self.wrapSpell(self.extractIconID(href)) + self.wrapTextWith(spellName, self.spellColor)).rstrip('\n')
+
+                contentTextList.append('- ' + text)
+
+        contentText = ''.join(contentTextList)
+        return  contentText.rstrip()
+
+    def finalize(self, bossName, tankSectionTag, healerSectionTag, dpsSectionTag):
+        tankContentList = tankSectionTag.next_element.next_element.next_element
+        healerContentList = healerSectionTag.next_element.next_element.next_element
+        dpsContentList = dpsSectionTag.next_element.next_element.next_element
+
+        # title
+        textList = []
+        textList.append(self.wrapTextWith(bossName, self.redColor) + '\n')
+
+        # content
+        tankText = '{T}' + self.wrapTextWith(tankSectionTag.string, self.greenColor) + self.contentPrepare(tankContentList) + '{/T}'
+        textList.append(tankText)
+
+        healerText = '{H}' + self.wrapTextWith(healerSectionTag.string, self.greenColor) + self.contentPrepare(healerContentList) + '{/H}'
+        textList.append(healerText)
+
+        dpsText = '{D}' + self.wrapTextWith(dpsSectionTag.string, self.greenColor) + self.contentPrepare(dpsContentList) + '{/D}'
+        textList.append(dpsText)
+
+        return ''.join(textList)
+
+
+
+if __name__ == '__main__':
+    url = 'https://www.icy-veins.com/wow/orgozoa-strategy-guide-in-the-eternal-palace-raid'
+    Converter(url)
+
+
+
+
