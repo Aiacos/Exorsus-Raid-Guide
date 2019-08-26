be8a943fad990466d219c44a0645feb42342d5c9
Lorenzo <uni.lorenzo.a@gmail.com>
2019-08-26 19:06:41 +0100

    cleanUp 1

--- script/main.py
+++ script/main.py
@@ -1,15 +1,15 @@
 from bs4 import BeautifulSoup as bs
 import requests
-from Parser import TactParser
+from Parser import TactParser, BossParser
 from Converter import Converter
 
 
 if __name__ == '__main__':
-    url = 'https://www.icy-veins.com/wow/queen-azshara-strategy-guide-in-the-eternal-palace-raid'
-    url2 = 'https://www.icy-veins.com/wow/orgozoa-strategy-guide-in-the-eternal-palace-raid'
-    url3 = 'https://www.icy-veins.com/wow/za-qul-harbinger-of-ny-alotha-strategy-guide-in-the-eternal-palace-raid'
+    url = 'https://www.icy-veins.com/wow/the-queens-court-strategy-guide-in-the-eternal-palace-raid'
+    url2 = 'https://www.icy-veins.com/wow/za-qul-harbinger-of-ny-alotha-strategy-guide-in-the-eternal-palace-raid'
+    url3 = 'https://www.icy-veins.com/wow/queen-azshara-strategy-guide-in-the-eternal-palace-raid'
 
-    p = TactParser(url2)
+    p = TactParser(url3)
     c = Converter(p.getBossTactTagDict())
     print c.get_text()
 
