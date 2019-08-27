4ea0d3d837959c75da7cd66a381ca2e2f25e2fec
Lorenzo <uni.lorenzo.a@gmail.com>
2019-08-27 20:00:11 +0100

    update main and Boss

--- script/main.py
+++ script/main.py
@@ -5,16 +5,11 @@ from Converter import Converter
 
 
 if __name__ == '__main__':
-    url1 = 'https://www.icy-veins.com/wow/abyssal-commander-sivara-strategy-guide-in-the-eternal-palace-raid'
     url = 'https://www.icy-veins.com/wow/the-queens-court-strategy-guide-in-the-eternal-palace-raid'
     url2 = 'https://www.icy-veins.com/wow/za-qul-harbinger-of-ny-alotha-strategy-guide-in-the-eternal-palace-raid'
     url3 = 'https://www.icy-veins.com/wow/queen-azshara-strategy-guide-in-the-eternal-palace-raid'
 
-    b = BossParser(url)
-    boss = b.getBossList()[0][1]
-    print boss
-
-    p = TactParser(boss)
+    p = TactParser(url1)
     c = Converter(p.getBossTactTagDict())
     print c.get_text()
 
